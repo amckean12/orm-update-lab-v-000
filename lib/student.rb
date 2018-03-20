ed5fae42edb1dc8dbe2040de09bbe3f7bb38b3a5
Alex McKean <amckean@me.com>
2018-03-20 13:19:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ end
 def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Students.new(result[0], result[1], result[2])
+    Student.new(result[0], result[1], result[2])
 end
 
 
